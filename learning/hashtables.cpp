596c7a8d630d01373007e34387136edca9cccc67
thehman57 <159841199+thehman57@users.noreply.github.com>
2024-09-22 02:54:59 +0000

    Sat, Sep 21, 2024, 9:54 PM -05:00

--- learning/hashtables.cpp
+++ learning/hashtables.cpp
@@ -11,3 +11,69 @@ typedef struct node
     struct node *next;
     struct node *tables[26];
 } node;
+
+unsigned int key(string name);
+void append();
+void output();
+
+node *list[26] = {NULL};
+
+int main()
+{
+    char cont = 'Y';
+    cout << "This is a test to see my knowledge of making hashtables." << endl << endl;
+
+    do
+    {
+        cout << "Would you like to add a name? (Y/N)" << endl;
+        cin >> cont;
+        cont = toupper(cont);
+
+        if (cont == 'Y')
+        {
+            append();
+        }
+
+    } while (cont == 'Y');
+
+    do
+    {
+        cout << "Would you like to see a letter section?" << endl;
+        cin >> cont;
+        cont = toupper(cont);
+
+        if (cont == 'Y')
+        {
+            output();
+        }
+
+    } while (cont == 'Y');
+
+    return 0;
+}
+
+unsigned int key(string name)
+{
+    return toupper(name[0]) - 'A';
+}
+
+void append()
+{
+    int key;
+    string name;
+
+    cout << "Please input a name for the list:" << endl;
+    cin >> name;
+
+    key = key(name);
+
+    node *n = (node*)malloc(sizeof(node));
+
+    n->name[key] = name;
+    n->
+}
+
+void output()
+{
+
+}
