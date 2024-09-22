73e48c1c7664100ec0c21148dfecab5d2e6fccd6
thehman57 <159841199+thehman57@users.noreply.github.com>
2024-09-22 01:01:56 +0000

    Sat, Sep 21, 2024, 8:01 PM -05:00

--- learning/hashtables.c
+++ learning/hashtables.c
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+//structure
 typedef struct node
 {
     struct node *table[26];
@@ -18,8 +19,12 @@ int main()
 
 }
 
-unsigned int keygen()
+unsigned int keygen(string name)
 {
 
 }
 
+void append(string name, int key)
+{
+
+}
