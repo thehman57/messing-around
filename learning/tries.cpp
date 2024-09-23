3516f223dd3c9092eb3a522f4a8d6832a55b5dad
thehman57 <159841199+thehman57@users.noreply.github.com>
2024-09-23 20:55:41 +0000

    Mon, Sep 23, 2024, 3:55 PM -05:00

--- learning/tries.cpp
+++ learning/tries.cpp
@@ -6,8 +6,15 @@
 
 using namespace std;
 
+// trying to make a trie data structure
 typedef struct trie
 {
-    string word[45];
+    string word[MAX_LENGTH];
     struct trie *paths[26];
-}
+    bool isEnd = false;
+} trie;
+
+
+//global variables
+trie *list[45][26] = {NULL};
+
