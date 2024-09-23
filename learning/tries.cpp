dd504b094ccd10679c41966c947e901222916317
thehman57 <159841199+thehman57@users.noreply.github.com>
2024-09-23 21:01:05 +0000

    Mon, Sep 23, 2024, 4:01 PM -05:00

--- learning/tries.cpp
+++ learning/tries.cpp
@@ -6,15 +6,25 @@
 
 using namespace std;
 
+
+// longest word, const allows you to use it for arrays
+const int MAX_LENGTH = 45; // pneumonoultramicroscopicsilicovolcanoconiosis is 45 letters long
+
 // trying to make a trie data structure
 typedef struct trie
 {
     string word[MAX_LENGTH];
-    struct trie *paths[26];
+    struct trie *paths[26]; // 26 is the amount of letters in the alphabet
     bool isEnd = false;
 } trie;
 
 
-//global variables
-trie *list[45][26] = {NULL};
+//global variable
+trie *list[MAX_LENGTH][26] = {NULL};
+
+
+int main()
+{
+    cout << "Hello World!" << endl;
+}
 
