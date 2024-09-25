1402cd3801cbf8478abd49ba9bfdc93a8b194a6a
thehman57 <159841199+thehman57@users.noreply.github.com>
2024-09-25 23:53:16 +0000

    Wed, Sep 25, 2024, 6:53 PM -05:00

--- learning/queues.cpp
+++ learning/queues.cpp
@@ -1,3 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+using namespace std;
+
+// use this for readability + information later
+const int CAPACITY = 10;
+
+typedef struct queue
+{
+    VALUE array[CAPACITY];
+    int front;
+    int size;
+}queue;
+
+queue *
